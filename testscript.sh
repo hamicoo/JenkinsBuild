0bab7c076defc1cf133350fe85c5bd666bb06d1f
Hamed Azizi vamerzani <perance.persia@gmail.com>
2022-11-06 17:49:07 +0100

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,6 @@
 echo "this is a script from Github"
-OPRATOR = '+'
- if [ "$OPRATOR"  == "+" ]; then
+OPRATOR = $OP
+ if [ "$OPRATOR"  = "+" ]; then
   NUMBER=$(($NUMBERONE + $NUMBERTWO))  
   echo "$NUMBER"
   echo "this task completed"
